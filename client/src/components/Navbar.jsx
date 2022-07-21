c5c79178fd2a0521700a9e6b186937e57fc205dc
Adesh Khole <adeshkhole8686@gmail.com>
2022-07-22 00:46:13 +0530

    Add png

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import {HiMenuAlt4} from 'react-icons/hi';
 import {AiOutlineClose} from 'react-icons/ai';
-import logo from '../components/images/logo.png';
+import logo from '../../images/images/logo.png';
 
 const Navbar =() =>{
     return(
         <nav className="w-full flex md:justify-center justify-between items-center p-4">
-
+            <div className="md:flex-[0.5] flex-initial justify-center items-center"></div>
+            <img src="logo" alt="logo" className="w-32 cursor-pointer"/>
+<ul></ul>
         </nav>
     );
 }
