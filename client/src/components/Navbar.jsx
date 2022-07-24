a109aa0455b9ba6f89be2ae504dbdb1dfb6f8caa
Adesh Khole <adeshkhole8686@gmail.com>
2022-07-24 22:57:42 +0530

    write the image and define the logo at img src

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar =() =>{
     return(
         <nav className="w-full flex md:justify-center justify-between items-center p-4">
             <div className="md:flex-[0.5] flex-initial justify-center items-center"></div>
-            <img src="logo" alt="logo" className="w-32 cursor-pointer"/>
+            <img src={logo} alt="logo" className="w-32 cursor-pointer"/>
 <ul></ul>
         </nav>
     );
